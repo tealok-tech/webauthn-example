fd088558e39799a39c7cd55b96a60d691fce7f91
Eli Ribble <eli@theribbles.org>
2024-12-12 15:15:14 -0700

    Fix whitespace in JavaScript file

--- index.js
+++ index.js
@@ -1,140 +1,140 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  // check whether current browser supports WebAuthn
-  if (!window.PublicKeyCredential) {
-	alert("Error: this browser does not support WebAuthn");
-	return;
-  }
+	// check whether current browser supports WebAuthn
+	if (!window.PublicKeyCredential) {
+		alert("Error: this browser does not support WebAuthn");
+		return;
+	}
 });
 
 // Base64 to ArrayBuffer
 function bufferDecode(value) {
-  return Uint8Array.from(atob(value), c => c.charCodeAt(0));
+	return Uint8Array.from(atob(value), c => c.charCodeAt(0));
 }
 
 // ArrayBuffer to URLBase64
 function bufferEncode(value) {
-  return btoa(String.fromCharCode.apply(null, new Uint8Array(value)))
-	.replace(/\+/g, "-")
-	.replace(/\//g, "_")
-	.replace(/=/g, "");;
+	return btoa(String.fromCharCode.apply(null, new Uint8Array(value)))
+		.replace(/\+/g, "-")
+		.replace(/\//g, "_")
+		.replace(/=/g, "");;
 }
 
 function registerUser() {
-  const username = document.querySelector("#email").value;
-  if (username === "") {
-	alert("Please enter a username");
-	return;
-  }
+	const username = document.querySelector("#email").value;
+	if (username === "") {
+		alert("Please enter a username");
+		return;
+	}
 
-  fetch('/register/begin/' + username)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error on begin register: ${response.status}`);
-      }
-      return response.json();
-    }).then((credentialCreationOptions) => {
-	  console.log(credentialCreationOptions)
-	  credentialCreationOptions.publicKey.challenge = bufferDecode(credentialCreationOptions.publicKey.challenge);
-	  credentialCreationOptions.publicKey.user.id = bufferDecode(credentialCreationOptions.publicKey.user.id);
-	  if (credentialCreationOptions.publicKey.excludeCredentials) {
-		for (var i = 0; i < credentialCreationOptions.publicKey.excludeCredentials.length; i++) {
-		  credentialCreationOptions.publicKey.excludeCredentials[i].id = bufferDecode(credentialCreationOptions.publicKey.excludeCredentials[i].id);
-		}
-	  }
+	fetch('/register/begin/' + username)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error(`HTTP error on begin register: ${response.status}`);
+			}
+			return response.json();
+		}).then((credentialCreationOptions) => {
+			console.log(credentialCreationOptions)
+			credentialCreationOptions.publicKey.challenge = bufferDecode(credentialCreationOptions.publicKey.challenge);
+			credentialCreationOptions.publicKey.user.id = bufferDecode(credentialCreationOptions.publicKey.user.id);
+			if (credentialCreationOptions.publicKey.excludeCredentials) {
+				for (var i = 0; i < credentialCreationOptions.publicKey.excludeCredentials.length; i++) {
+					credentialCreationOptions.publicKey.excludeCredentials[i].id = bufferDecode(credentialCreationOptions.publicKey.excludeCredentials[i].id);
+				}
+			}
 
-	  return navigator.credentials.create({
-		publicKey: credentialCreationOptions.publicKey
-	  })
-	})
-	.then((credential) => {
-	  console.log(credential)
-	  let attestationObject = credential.response.attestationObject;
-	  let clientDataJSON = credential.response.clientDataJSON;
-	  let rawId = credential.rawId;
-
-      fetch( '/register/finish/' + username, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-		  id: credential.id,
-		  rawId: bufferEncode(rawId),
-		  type: credential.type,
-		  response: {
-			attestationObject: bufferEncode(attestationObject),
-			clientDataJSON: bufferEncode(clientDataJSON),
-		  },
+			return navigator.credentials.create({
+				publicKey: credentialCreationOptions.publicKey
+			})
 		})
-	  })
-   })
-  .then((success) => {
-	    alert("successfully registered " + username + "!")
-    return
-  })
-  .catch((error) => {
-    console.log(error)
-    alert("failed to register " + username)
-  })
+		.then((credential) => {
+			console.log(credential)
+			let attestationObject = credential.response.attestationObject;
+			let clientDataJSON = credential.response.clientDataJSON;
+			let rawId = credential.rawId;
+
+			fetch( '/register/finish/' + username, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					id: credential.id,
+					rawId: bufferEncode(rawId),
+					type: credential.type,
+					response: {
+						attestationObject: bufferEncode(attestationObject),
+						clientDataJSON: bufferEncode(clientDataJSON),
+					},
+				})
+			})
+	 })
+	.then((success) => {
+			alert("successfully registered " + username + "!")
+		return
+	})
+	.catch((error) => {
+		console.log(error)
+		alert("failed to register " + username)
+	})
 }
 
 function loginUser() {
-  const username = document.querySelector("#email").value;
-  if (username === "") {
-	alert("Please enter a username");
-	return;
-  }
+	const username = document.querySelector("#email").value;
+	if (username === "") {
+		alert("Please enter a username");
+		return;
+	}
 
-  fetch('/login/begin/' + username)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error on begin register: ${response.status}`);
-      }
-      return response.json();
-    }).then((credentialRequestOptions) => {
-	  console.log(credentialRequestOptions)
-	  credentialRequestOptions.publicKey.challenge = bufferDecode(credentialRequestOptions.publicKey.challenge);
-	  credentialRequestOptions.publicKey.allowCredentials.forEach(function (listItem) {
-		listItem.id = bufferDecode(listItem.id)
-	  });
+	fetch('/login/begin/' + username)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error(`HTTP error on begin register: ${response.status}`);
+			}
+			return response.json();
+		}).then((credentialRequestOptions) => {
+			console.log(credentialRequestOptions)
+			credentialRequestOptions.publicKey.challenge = bufferDecode(credentialRequestOptions.publicKey.challenge);
+			credentialRequestOptions.publicKey.allowCredentials.forEach(function (listItem) {
+				listItem.id = bufferDecode(listItem.id)
+			});
 
-	  return navigator.credentials.get({
-		publicKey: credentialRequestOptions.publicKey
-	  })
-	})
-	.then((assertion) => {
-	  console.log(assertion)
-	  let authData = assertion.response.authenticatorData;
-	  let clientDataJSON = assertion.response.clientDataJSON;
-	  let rawId = assertion.rawId;
-	  let sig = assertion.response.signature;
-	  let userHandle = assertion.response.userHandle;
-
-      fetch( '/login/finish/' + username, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-		  id: assertion.id,
-		  rawId: bufferEncode(rawId),
-		  type: assertion.type,
-		  response: {
-			authenticatorData: bufferEncode(authData),
-			clientDataJSON: bufferEncode(clientDataJSON),
-			signature: bufferEncode(sig),
-			userHandle: bufferEncode(userHandle),
-		  },
+			return navigator.credentials.get({
+				publicKey: credentialRequestOptions.publicKey
+			})
 		})
-       })
-	})
-	.then((success) => {
-	  alert("successfully logged in " + username + "!")
-	  return
-	})
-	.catch((error) => {
-	  console.log(error)
-	  alert("failed to login " + username)
-	})
+		.then((assertion) => {
+			console.log(assertion)
+			let authData = assertion.response.authenticatorData;
+			let clientDataJSON = assertion.response.clientDataJSON;
+			let rawId = assertion.rawId;
+			let sig = assertion.response.signature;
+			let userHandle = assertion.response.userHandle;
+
+			fetch( '/login/finish/' + username, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					id: assertion.id,
+					rawId: bufferEncode(rawId),
+					type: assertion.type,
+					response: {
+						authenticatorData: bufferEncode(authData),
+						clientDataJSON: bufferEncode(clientDataJSON),
+						signature: bufferEncode(sig),
+						userHandle: bufferEncode(userHandle),
+					},
+				})
+			 })
+		})
+		.then((success) => {
+			alert("successfully logged in " + username + "!")
+			return
+		})
+		.catch((error) => {
+			console.log(error)
+			alert("failed to login " + username)
+		})
 }
 
